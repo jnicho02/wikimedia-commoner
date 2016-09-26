3f4abe1bc1bde1d186ffafb0cb51cec2f12facd7
Jez Nicholson <jez.nicholson@gmail.com>
2016-09-26 15:30:27 +0100

    add positive description test

--- spec/description_spec.rb
+++ spec/description_spec.rb
@@ -10,5 +10,14 @@ describe Wikimedia::Commoner do
         end
       end
     end
+
+    context 'of a Delhi portrait of a man' do
+      it 'should be there' do
+        VCR.use_cassette ('details/' + self.class.description).gsub(" ","-") do
+        image = Wikimedia::Commoner.details 'File:India_-_Delhi_portrait_of_a_man_-_4780.jpg'
+          expect(image[:description]).to include 'English: Portrait of a man, Delhi, India.'
+        end
+      end
+    end
   end
 end
