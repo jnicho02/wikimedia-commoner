9696643a932d4dd30324e7ee23cdcd5080b5cf43
Jez Nicholson <jez.nicholson@gmail.com>
2018-12-05 18:50:34 +0000

    handle cc-by-sa-4.0

--- spec/licence_spec.rb
+++ spec/licence_spec.rb
@@ -16,9 +16,9 @@ describe Wikimedia::Commoner do
 
     context 'of Nahal Zaror, south 11' do
       let(:image) {
-#        VCR.use_cassette("details/#{self.class.description}".gsub(" ","-")) {
+        VCR.use_cassette("details/#{self.class.description}".gsub(" ","-")) {
           Wikimedia::Commoner.details('File:Nahal_Zaror,_south_11.jpg')
-#        }
+        }
       }
       it 'should be CC BY-SA 3.0' do
         expect(image[:licence]).to eq('CC BY-SA 3.0')
@@ -57,7 +57,7 @@ describe Wikimedia::Commoner do
         }
       }
       it 'should be CC-PD-Mark' do
-        expect(image[:licence]).to eq('CC-PD-Mark')
+        expect(image[:licence]).to eq('Public domain')
       end
       it 'should link to the public domain licence page' do
         expect(image[:licence_url]).to eq('https://creativecommons.org/publicdomain/mark/1.0')
